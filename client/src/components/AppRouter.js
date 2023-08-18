4cee2d354e090ffa25b4cc961cd80f89f2bb4f61
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-08-18 17:00:49 -0400

    Added clien project and first redirection for user and admin

---
+++ client/src/components/AppRouter.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Route, Navigate, Routes } from "react-router-dom";
+import { authRoutes, publicRoutes } from "../routes";
+import { SHOP_ROUTE } from "../utils/consts";
+
+const AppRouter = () => {
+  const isAuth = true;
+  return (
+    <Routes>
+      {isAuth &&
+        authRoutes.map(({ path, Component }) => (
+          <Route key={path} path={path} element={<Component />} />
+        ))}
+      {publicRoutes.map(({ path, Component }) => (
+        <Route key={path} path={path} element={<Component />} />
+      ))}
+      <Route path="*" element={<Navigate to={SHOP_ROUTE} />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
---
+++ client/src/pages/Shop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Shop = () => {
+  return <div className="">Shop</div>;
+};
+
+export default Shop;
